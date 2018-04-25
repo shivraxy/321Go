7a8f642eb854c7b38563180408aa38c4ef0ab26e
Alex <alovell@salesforce.com>
2018-04-24 19:53:45 -0700

    added update and destroy for GroupEvents

--- server/controllers/groupevents.js
+++ server/controllers/groupevents.js
@@ -10,4 +10,53 @@ module.exports = {
             .then(groupEvent => res.status(201).send(groupEvent))
             .catch(error => res.status(400).send(error));
     },
+
+    update(req, res) {
+        return GroupEvent
+            .find({
+                where: {
+                    id: req.params.groupEventId,
+                    groupId: req.params.groupId,
+                },
+            })
+            .then(groupEvent => {
+                if(!groupEvent) {
+                    return res.status(404).send({
+                        message: 'GroupEvent not found',
+                    });
+                }
+            return groupEvent
+                .update({
+                    // consider changing to something more scaleable like: .update(req.body, { fields: Object.keys(req.body) })
+                    content: req.body.content || groupEvent.content,
+                    complete: req.body.complete || groupEvent.complete,
+                })
+                .then(updatedGroupEvent => res.status(200).send(updatedGroupEvent))
+                .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
+    },
+
+    destroy(req, res) {
+        return GroupEvent
+            .find({
+                where: {
+                    id: req.params.groupEventId,
+                    groupId: req.params.groupId,
+                },
+            })
+            .then(groupEvent => {
+                if (!groupEvent) {
+                    return res.status(404).send({
+                        message: 'GroupEvent not found',
+                    });
+                }
+
+                return groupEvent
+                    .destroy()
+                    .then(() => res.status(204).send())
+                    .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
+    },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,10 +8,19 @@ module.exports = app => {
 
     app.post('/api/groups', groupsController.create);
     app.get('/api/groups', groupsController.list);
-    
+
     app.get('/api/groups/:groupId', groupsController.retrieve);
     app.put('/api/groups/:groupId', groupsController.update);
     app.delete('/api/groups/:groupId', groupsController.destroy);
 
     app.post('/api/groups/:groupId/groupevents', groupEventsController.create);
+
+    app.put('/api/groups/:groupId/events/:groupEventId', groupEventsController.update);
+    app.delete('/api/groups/:groupId/events/:groupEventId', groupEventsController.destroy);
+
+    // don't allow any other request methods
+    app.all('/api/groups/:groupId/events', (req, res) =>
+        res.status(405).send({
+            message: 'Method not allowed',
+    }));
 };
