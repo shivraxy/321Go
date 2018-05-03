d645def5cda42ad8649970c29f696549047f7ed2
Jim Brashear <returntosender@earthlink.net>
2018-05-03 01:12:20 -0700

    deleted irrelevant group list files--will be replaced by table

--- src/Components/Groups/GroupList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GroupList = props => (
-    <ul className="list-group">{props.children}</ul>
-);
-
-export default GroupList;
--- src/Components/Groups/GroupListItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GroupListItem = props => (
-    <li className="list-item" onClick={props.handleClick}>{props.name}</li>
-);
-
-export default GroupListItem;
