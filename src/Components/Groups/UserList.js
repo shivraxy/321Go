213c0f2de275bffbb7e8270755d6eba6f3a19eec
Jim Brashear <returntosender@earthlink.net>
2018-05-03 01:08:53 -0700

    deleted irrelevant user files

--- src/Components/Groups/UserList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserList = props => (
-    <ul className="list-group">{props.children}</ul>
-);
-
-export default UserList;
--- src/Components/Groups/UserListItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserListItem = props => (
-    <li className="list-item">{props.name}</li>
-);
-
-export default UserListItem;
