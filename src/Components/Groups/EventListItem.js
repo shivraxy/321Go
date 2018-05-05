19ff8107594272c527e07a12ffb8ce7419e2b826
Jim Brashear <returntosender@earthlink.net>
2018-05-04 23:42:32 -0700

    added checkbox component

--- src/Components/Groups/EventListItem.js
+++ src/Components/Groups/EventListItem.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Checkbox from "./Checkbox";
 require('./EventListItem.css');
+
 
 const EventListItem = props => {
     console.log('props in EventListItem', props);
     return (
         <li className="list-item" onClick={props.handleClick && props.handleClick}>
             <a href={props.url}>{props.name.text}</a>
+            <Checkbox />
         </li>
     )
 };
